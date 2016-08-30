d59240453c1966d21fad0e51640f2dbf41eceb37
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-08-30 09:08:46 +0900

    add PPA
    the official package is too old (1.5.x)

--- scripts/ubuntu/apt.sh
+++ scripts/ubuntu/apt.sh
@@ -10,4 +10,9 @@ if [ "$ubuntu_version" == '12.04' ]; then
 fi
 
 sudo apt-get update
+
+# install the latest ansible from ppa
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
 sudo apt-get -y install ansible rsync
