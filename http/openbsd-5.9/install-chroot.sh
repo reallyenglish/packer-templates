a4ccabfad20191a4fcbe2101a37ad24f98bff123
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-02 12:48:23 +0900

    restore 5.9, which has been renamed in upstream

---
+++ http/openbsd-5.9/install-chroot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+set -x
+
+pkg_add sudo--
+cat <<EOF > /etc/sudoers
+
+#includedir /etc/sudoers.d
+EOF
+mkdir /etc/sudoers.d
+cat <<EOF > /etc/sudoers.d/vagrant
+Defaults:vagrant !requiretty
+vagrant ALL=(ALL) NOPASSWD: ALL
+EOF
+chmod 440 /etc/sudoers.d/vagrant
+
+cat <<EOF > /etc/boot.conf
+set timeout 1
+EOF
