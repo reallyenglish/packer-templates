145e51d902d05ded8027d0bacbf4f3e73596d716
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-06-07 12:46:00 +0900

    add examples for openbsd

--- reallyenglish_spec/packages_spec.rb
+++ reallyenglish_spec/packages_spec.rb
@@ -7,3 +7,35 @@ packages.each do |p|
     it { should be_installed }
   end
 end
+
+case os[:family]
+when "openbsd"
+  describe file("/etc/pkg.conf") do
+    it { should exist }
+    it { should be_file }
+    it { should be_mode 644 }
+    its(:content) { should match(/^installpath\s*=\s*ftp\.openbsd\.org/) }
+  end
+
+  prefix = "/usr/local/bin"
+  python_version = "2.7"
+  python_symlinks = {
+    "#{prefix}/python#{python_version}" => "#{prefix}/python",
+    "#{prefix}/python#{python_version}-2to3" => "#{prefix}/2to3",
+    "#{prefix}/python#{python_version}-config" => "#{prefix}/python-config",
+    "#{prefix}/pydoc#{python_version}" => "#{prefix}/pydoc"
+  }
+  python_symlinks.each do |k, v|
+    describe file(k) do
+      it { should exist }
+      it { should be_symlink }
+      it { should be_linked_to v }
+    end
+
+    describe file(v) do
+      it { should exist }
+      it { should be_file }
+      it { should be_mode 755 }
+    end
+  end
+end
