8adcc6d39728003c62cc6f179a6d3db4970aa665
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-09-27 09:09:22 +0900

    allow root to use sudo
    this also updates packages to the latest

---
+++ reallyenglish_spec/sudo_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+cmd = ""
+case os[:family]
+when "freebsd"
+  cmd = "su -m root -c 'sudo echo'"
+when "openbsd", "debian", "ubuntu", "redhat"
+  cmd = "su -l -s /bin/sh root -c 'sudo echo'"
+end
+
+describe command(cmd) do
+  its(:exit_status) { should eq 0 }
+end
