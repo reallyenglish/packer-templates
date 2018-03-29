22dcbf367224c65442c5f86ec8052f33c6a57fc6
Mitsuru Y <mitsuruy@reallyenglish.com>
2018-03-29 14:06:05 +0900

    support single cpu OpenBSD box

--- reallyenglish_spec/cpus_spec.rb
+++ reallyenglish_spec/cpus_spec.rb
@@ -2,18 +2,34 @@ require "spec_helper"
 
 case os[:family]
 when "openbsd"
-  describe file("/bsd.sp") do
-    it { should exist }
-    it { should be_file }
-  end
+  if ENV['VAGRANT_CPU_CORE'] == "1"
+    describe file("/bsd.sp") do
+      it { should_not exist }
+    end
 
-  describe command("sysctl -n hw.ncpu") do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should eq "2\n" }
-  end
+    describe command("sysctl -n hw.ncpu") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should eq "1\n" }
+    end
 
-  describe command("sysctl -n hw.ncpufound") do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should eq "2\n" }
+    describe command("sysctl -n hw.ncpufound") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should eq "1\n" }
+    end
+  else
+    describe file("/bsd.sp") do
+      it { should exist }
+      it { should be_file }
+    end
+
+    describe command("sysctl -n hw.ncpu") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should eq "2\n" }
+    end
+
+    describe command("sysctl -n hw.ncpufound") do
+      its(:exit_status) { should eq 0 }
+      its(:stdout) { should eq "2\n" }
+    end
   end
 end
